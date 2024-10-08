c02bb1885120a2c1b1daeffa2309b28846339056
Markus Kari <markus.kari94@gmail.com>
2024-10-08 11:25:21 +0300

    Fixed the movement bug.

--- player.py
+++ player.py
@@ -120,20 +120,24 @@ class Player:
             possible.append("land")
         return possible
     
-    def destination_options(self, dice: int, ignore: str = None):
+    def destination_options(self, dice: int, loc: str = None, ignore: str = None):
         """Returns a list of all the locations player can go to.
 
         Args
         ----
         dice : int
             How many steps the player can take
+        loc: str, optional
+            If the function is calculated from another location than 
+            where the player is currently located. Defaults to None,
+            which refers to the player's current location.
         ignore : str, optional
             If there are any directions the player is not able to move
             towards. Defaults to None.
             E.g. player starts at "Cai-Egy-3-1" and rolls a two, this
-            function will be called recursively with dice=1, location
-            ="Egy" and ignore="Cai", because otherwise the player would
-            be able to go one step to Egypt and one step backwards,
+            function will be called recursively with dice=1, loc="Egy" 
+            and ignore="Cai", because otherwise the player would be
+            able to go one step to Egypt and one step backwards,
             ending in the same space where the player started, which is
             not allowed.
 
@@ -142,52 +146,35 @@ class Player:
             A list of location strings where the player can go to
         """
         options = []
-        locs = re.split(r"-", self.location)
+        if loc is None:
+            loc = self.location
+        locs = re.split(r"-", loc)
         # in a node
         if len(locs) == 1:
             if self.offshore:
-                for x in map.sea_routes[locs[0]]:
-                    if x[0] != ignore:
-                        if x[1] > dice:
-                            options.append(
-                                locs[0]
-                                + "-"
-                                + x[0]
-                                + "-"
-                                + str(dice)
-                                + "-"
-                                + str(x[1] - dice)
-                            )
-                        elif x[1] == dice:
-                            options.append(x[0])
-                        else:
-                            if "nd" in x[0]:
-                                options = options + self.destination_options(
-                                    dice - x[1], self.location
-                                )
-                            else:
-                                options.append(x[0])
+                dest = [x for x in map.sea_routes[loc] if x[0] != ignore]
             else:
-                for x in map.land_routes[locs[0]]:
-                    if x[0] != ignore:
-                        if x[1] > dice:
-                            options.append(
-                                locs[0]
-                                + "-"
-                                + x[0]
-                                + "-"
-                                + str(dice)
-                                + "-"
-                                + str(x[1] - dice)
-                            )
-                        elif x[1] == dice:
-                            options.append(x[0])
-                        else:
-                            if "nd" not in x[0]:
-                                options.append(x[0])
-                            options = options + self.destination_options(
-                                dice - x[1], self.location
-                            )
+                dest = [x for x in map.land_routes[loc] if x[0] != ignore]
+            for x in dest:
+                if x[1] > dice:
+                    options.append(
+                        locs[0]
+                        + "-"
+                        + x[0]
+                        + "-"
+                        + str(dice)
+                        + "-"
+                        + str(x[1] - dice)
+                    )
+                elif x[1] == dice:
+                    options.append(x[0])
+                else:
+                    if "nd" not in x[0]:
+                        options.append(x[0])
+                    if not self.offshore or "nd" in x[0]:
+                        options = options + self.destination_options(
+                            dice - x[1], x[0], loc
+                        )
         # between nodes
         else:
             # towards the town player came from (locs[0])
@@ -204,18 +191,11 @@ class Player:
             elif int(locs[2]) == dice:
                 options.append(locs[0])
             else:
-                if self.offshore:
-                    if "nd" not in locs[0]:
-                        options.append(locs[0])
-                    else:
-                        options = options + self.destination_options(
-                            dice - int(locs[2]), locs[1]
-                        )
-                else:
-                    if "nd" not in locs[0]:
-                        options.append(locs[0])
+                if "nd" not in locs[0]:
+                    options.append(locs[0])
+                if not self.offshore or "nd" in locs[0]:
                     options = options + self.destination_options(
-                        dice - int(locs[2]), locs[1]
+                        dice - int(locs[2]), locs[0], locs[1]
                     )
             # towards the town the player is heading (locs[1])
             if int(locs[3]) > dice:
@@ -231,18 +211,11 @@ class Player:
             elif int(locs[3]) == dice:
                 options.append(locs[1])
             else:
-                if self.offshore:
-                    if "nd" not in locs[1]:
-                        options.append(locs[1])
-                    else:
-                        options = options + self.destination_options(
-                            dice - int(locs[3]), locs[0]
-                        )
-                else:
-                    if "nd" not in locs[1]:
-                        options.append(locs[1])
+                if "nd" not in locs[1]:
+                    options.append(locs[1])
+                if not self.offshore or "nd" in locs[1]:
                     options = options + self.destination_options(
-                        dice - int(locs[3]), locs[0]
+                        dice - int(locs[3]), locs[1], locs[0]
                     )
         return options
     
